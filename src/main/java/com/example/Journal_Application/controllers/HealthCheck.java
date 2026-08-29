30f6273e555ef7c9626bf0414a16b3541c355511
Dhruv Gupta <dhruvg308@gmail.com>
2026-08-30 00:51:56 +0530

    Created HealthCheck Controller

---
+++ src/main/java/com/example/Journal_Application/controllers/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.example._3_journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health-check")
+    public String healthCheack(){
+        return "OK";
+    }
+}
