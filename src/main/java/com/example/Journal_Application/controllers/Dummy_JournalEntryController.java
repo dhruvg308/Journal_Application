89d48536455430caf0521341b54bcde4a914b7f8
Dhruv Gupta <dhruvg308@gmail.com>
2026-08-31 12:11:58 +0530

    Dummy controller for journal entries

---
+++ src/main/java/com/example/Journal_Application/controllers/Dummy_JournalEntryController.java
@@ -0,0 +1,46 @@
+// Dummy journalController without any Database connection and Without Service connection
+// Using JournalController_v2
+package com.example._3_journalApp.controller;
+
+import com.example._3_journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/dummy-journal")
+public class Dummy_JournalEntryController {
+
+    private Map<ObjectId, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable String myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable String myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId, myEntry);
+    }
+
+}
